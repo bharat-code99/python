1cd22d568aadfd5257c2c49edf5df4449459af57
bharat-code99 <2491bhr@gmail.com>
2025-12-28 22:03:33 +0530

    clean up

--- test.py
+++
@@ -1,7 +0,0 @@
-
-matrix = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
-
-for index,row in enumerate(matrix):
-    print(' | '.join(row))
-    if (index+1) < len(matrix):
-        print("---------")
